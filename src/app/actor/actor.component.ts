d2ccf9b4a2097ee5ebd7cf10a5950ccee91d6db0
Ak0s445 <cipolakacsoakos5@gmail.com>
2025-09-25 10:55:26 +0200

    Elso valtozat kesz

--- src/app/actor/actor.component.ts
+++ src/app/actor/actor.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../shared/api.service';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-actor',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './actor.component.html',
   styleUrl: './actor.component.css'
 })
 export class ActorComponent {
   actors: any;
+  actorForm: any;
 
-
-
-
-  constructor(private api:ApiService){}
+  constructor(private api:ApiService,
+    private builder: FormBuilder //formbuilderrel dolgozunk
+  ){}
 
 ngOnInit(){
   this.getActors()
+  this.actorForm = this.builder.group({
+    //objektumként kell megadni a mezőket
+
+    name: '',
+    city: '',
+    birthDate: ''
+    //kezdetben ezek most lehetnek üresek
+  })
 }
 
   getActors(){
@@ -28,4 +37,16 @@ ngOnInit(){
       },
     })
   }
+  addActor() {
+    console.log(this.actorForm.value) //megnézzük a submit esemény működik-e
+    this.api.addActor(this.actorForm.value).subscribe({
+      next: (result) => {
+        console.log(result) //kiirjuk mit ad vissza a szerver
+        this.getActors()
+        this.actorForm.reset() //ez törli automatikusan a tartalmat
+
+      }
+    })
+  }
+
 }
